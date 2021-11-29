a00aaef71d6b7995f6260a5868d85230126ede32
PriyankaArikatla1205 <90396220+PriyankaArikatla1205@users.noreply.github.com>
2021-11-29 10:28:19 +0530

    Add files via upload

---
+++ insertion.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+public class insertion {
+ 
+
+     
+    public static void main(String[]args){
+    int n,i,j,k;
+   
+    Scanner input= new Scanner(System.in);
+    n=input.nextInt();
+    int[]arr=new int[n];
+    for( i=0;i<n;i++){
+        arr[i]=input.nextInt();
+    }
+   
+    for(i=0;i<n-1;i++){
+       for(j=i+1;j>0;j--){
+           if(arr[j]<arr[j-1]){
+               k=arr[j];
+               arr[j]=arr[j-1];
+               arr[j-1]=k;
+           }
+       }
+    }
+    for(i=0;i<n;i++){
+        System.out.println(arr[i]);
+    }
+
+    
+    
+}
+}
